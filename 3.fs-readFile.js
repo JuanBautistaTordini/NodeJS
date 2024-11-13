95bb5b6026e3b94398c1be7370e8dff0b9a501c3
BautistaTordini <bautistatordini.uns@gmail.com>
2024-11-13 20:05:11 -0300

    chore: Update fs-readFile.js and fs-async-await.mjs

--- 3.fs-readFile.js
+++ 3.fs-readFile.js
@@ -1,13 +1,13 @@
-const fs = require('node:fs');
+const fs = require('node:fs')
 
-console.log('Leyendo el archivo...');
-fs.readFile('./archivo.txt', 'utf-8', (err, text) => {
-    console.log('primer texto:', text)
+console.log('Leyendo el primer archivo...')
+fs.readFile('./archivo.txt', 'utf-8', (err, text) => { // <---- ejecutas este callback
+  console.log('primer texto:', text)
 })
 
-console.log('Hacer Cosas mientras leer el archivo...');
+console.log('--> Hacer cosas mientras lee el archivo...')
 
-console.log('Leytondo el archivo...');
-fs.readFile('./archivo2.txt', 'utf-8', (err, secondtext) => {
-    console.log('segundo texto:', secondtext);
-});
+console.log('Leyendo el segundo archivo...')
+fs.readFile('./archivo2.txt', 'utf-8', (err, text) => {
+  console.log('segundo texto:', text)
+})
--- 5.fs-async-await.mjs
+++ 5.fs-async-await.mjs
@@ -1,29 +1,16 @@
-import { readFile } from 'node:fs/promises';
+// Esto sólo en los módulos nativos
+// que no tienen promesas nativas
+
+// const { promisify } = require('node:util')
+// const readFilePromise = promisify(fs.readFile)
+
+import { readFile } from 'node:fs/promises'
 
 console.log('Leyendo el primer archivo...')
-
 const text = await readFile('./archivo.txt', 'utf-8')
-console.log('primer archivo:', text)
+console.log('primer texto:', text)
+console.log('--> Hacer cosas mientras lee el archivo...')
 
-console.log('--> Hacer Cosas mientras leer el archivo...');
-
-console.log('Leyendo segundo archivo...');
-const secondtext = await readFile('./archivo2.txt', 'utf-8')
-console.log('segundo archivo:', secondtext);
-
-
-// IIFE - Immediately Invoked Function Expression
-
-// (async () => {
-//     console.log('Leyendo el primer archivo...')
-    
-//     const text = await readFile('./archivo.txt', 'utf-8')
-//     console.log('primer archivo:', text)
-    
-//     console.log('--> Hacer Cosas mientras leer el archivo...');
-    
-//     console.log('Leyendo segundo archivo...');
-//     const secondtext = await readFile('./archivo2.txt', 'utf-8')
-//     console.log('segundo archivo:', secondtext);
-//     }
-// )();
+console.log('Leyendo el segundo archivo...')
+const secondText = await readFile('./archivo2.txt', 'utf-8')
+console.log('segundo texto:', secondText)
